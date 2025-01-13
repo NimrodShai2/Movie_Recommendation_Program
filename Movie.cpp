98f41019d345573dd0643776d6c90068b5ffb257
nimni <nimni@LAPTOP-N14LBO6S>
2025-01-13 20:11:21 +0200

    Created the movie class

--- Movie.cpp
+++ Movie.cpp
@@ -1,5 +1,6 @@
 
 #include "Movie.h"
+
 #define HASH_START 17
 #define RES_MULT 31
 
@@ -8,7 +9,7 @@
  * @param movie shared pointer to movie
  * @return an integer for the hash map
  */
-std::size_t sp_movie_hash(const sp_movie& movie){
+std::size_t sp_movie_hash(const sp_movie &movie) {
     std::size_t res = HASH_START;
     res = res * RES_MULT + std::hash<std::string>()(movie->get_name());
     res = res * RES_MULT + std::hash<int>()(movie->get_year());
@@ -21,10 +22,52 @@ std::size_t sp_movie_hash(const sp_movie& movie){
  * @param m2
  * @return true if the year and name are equal else false
  */
-bool sp_movie_equal(const sp_movie& m1,const sp_movie& m2){
+bool sp_movie_equal(const sp_movie &m1, const sp_movie &m2) {
     return !(*m1 < *m2) && !(*m2 < *m1);
 }
 
+/**
+ * Constructor
+ * @param name name of the movie
+ * @param year year of the movie
+ */
+Movie::Movie(std::string name, int year) : name_(std::move(name)), year_(year) {}
+
+/**
+ * Get the name of the movie
+ * @return name of the movie
+ */
+std::string Movie::get_name() const {
+    return name_;
+}
+
+/**
+ * Get the year value of the movie
+ * @return Release year of the movie
+ */
+int Movie::get_year() const {
+    return year_;
+}
+
+/**
+ * Compare two movies by their release year and then name
+ * @param other movie to compare to
+ * @return true if this movie was released before the other movie or if they were released in the same year
+ * and the name of this movie is lexicographically smaller than the other movie
+ */
+bool Movie::operator<(const Movie &other) const {
+    return year_ < other.year_ || (year_ == other.year_ && name_ < other.name_);
+}
+
+/**
+ * Print the movie to the output stream
+ * @param os output stream
+ * @return output stream
+ */
+std::ostream &Movie::operator<<(std::ostream &os) const {
+    os << name_ << " (" << year_ << ")\n";
+    return os;
+}
 
 
 
--- Movie.h
+++ Movie.h
@@ -3,11 +3,11 @@
 #define EX5_MOVIE_H
 
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <memory>
 
 #define HASH_START 17
-
 
 
 class Movie;
@@ -17,15 +17,29 @@ typedef std::shared_ptr<Movie> sp_movie;
 /**
  * those declartions and typedefs are given to you and should be used in the ex
  */
-typedef std::size_t (*hash_func)(const sp_movie& movie);
-typedef bool (*equal_func)(const sp_movie& m1,const sp_movie& m2);
+typedef std::size_t (*hash_func)(const sp_movie &movie);
 
-std::size_t sp_movie_hash(const sp_movie& movie);
+typedef bool (*equal_func)(const sp_movie &m1, const sp_movie &m2);
 
-bool sp_movie_equal(const sp_movie& m1,const sp_movie& m2);
+std::size_t sp_movie_hash(const sp_movie &movie);
 
-class Movie
-{
+bool sp_movie_equal(const sp_movie &m1, const sp_movie &m2);
+
+class Movie {
+private:
+    /// name of the movie
+    std::string name_;
+    int year_;
+public:
+    Movie(std::string name, int year);
+
+    [[nodiscard]] std::string get_name() const;
+
+    [[nodiscard]] int get_year() const;
+
+    bool operator<(const Movie &other) const;
+
+    std::ostream &operator<<(std::ostream &os) const;
 
 };
 
